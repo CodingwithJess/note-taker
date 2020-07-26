03d4a62d5746307ccf732629eba3052a1306163e
CodingwithJess <thejessiebess@gmail.com>
2020-07-25 19:20:46 -0600

    fixed error where newNote was not appearing on page, needed to to .push into notes array

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
-const { allowedNodeEnvironmentFlags } = require("process");
 
 //express ports//
 const app = express();
@@ -13,7 +12,7 @@ app.use(express.static("public"));
 
 // global variables//
 let notes = [];
-let id = [];
+let id;
 
 const readNotes = () => {
   fs.readFile(__dirname + "/db/db.json", (err, data) => {
@@ -33,29 +32,45 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname,"/public/notes.html"));
 });
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname,"/public/index.html"));
 });
 
 //API route//
 app.get("/api/notes", (req,res) => {
   readNotes();
-  return res.json(notes);
+  res.json(notes);
 });
 
 // POST-should save note on req.body and add it to db.json then return new note to the client
-
 app.post("/api/notes", (req,res) => {
   newNote = req.body;
-  id = notes.length + 1,
-  newNote.id = id++,
+  id = notes.length + 1;
+  newNote.id = id++;
+
+  notes.push(newNote);
 
   writeNotes();
+  console.log("New note was written to DB!")
   res.json(notes);
+});
+
+app.delete("/api.notes/:id", (req, res) => {
+  var selectedID = parseInt(req.params.id);
+  let selectedNote = notes.find(note => note.id === selectedID);
+
+  notes.splice(notes.indexOf(selectedNote), 1);
+
+  writeNotes();
+  console.log("Note was deleted from DB.")
+
+  readNotes(); 
+
+  res.json(selectedNote);
 })
 
 
 app.listen(PORT, () => {
   readNotes();
   console.log("App listening on PORT" + PORT);
-})
+});
