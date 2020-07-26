0844cda929d9401185b5401e2f76bd219b2bac71
CodingwithJess <thejessiebess@gmail.com>
2020-07-25 18:34:10 -0600

    added API routes and readNotes function at global level

--- server.js
+++ server.js
@@ -15,16 +15,29 @@ app.use(express.static("public"));
 let notes = [];
 let id = [];
 
+const readNotes = () => {
+  fs.readFile(__dirname + "/db/db.json", (err, data) => {
+    if (err) throw err;
+    notes = JSON.parse(data);
+  });
+};
 
 // HTML routes//
 app.get("/notes", function (req, res){
   res.sendFile(path.join(__dirname,"/public/notes.html"));
-})
+});
 
 app.get("*", function (req, res){
   res.sendFile(path.join(__dirname,"/public/index.html"));
-})
+});
+
+//API route//
+app.get("/api/notes", function(req,res){
+  readNotes();
+  return res.json(notes);
+});
 
 app.listen(PORT, function (){
+  readNotes();
   console.log("App listening on PORT" + PORT);
 })
